1033b01b0b539914227211ab7c6a49c5102daff5
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-06-17 13:16:39 +0200

    Initial commit - Mirrored from my site, file: siege_mod_1710/src/main/java/hu/AfghanGoat/SiegeMod/Proxy/CommonProxy.java

---
+++ siege_mod_1710/src/main/java/hu/AfghanGoat/SiegeMod/Proxy/CommonProxy.java
@@ -0,0 +1,7 @@
+package hu.AfghanGoat.SiegeMod.Proxy;
+
+public class CommonProxy {
+    public void registerRenderers() {
+        // This is empty, as only the client proxy will need to register renderers.
+    }
+}
