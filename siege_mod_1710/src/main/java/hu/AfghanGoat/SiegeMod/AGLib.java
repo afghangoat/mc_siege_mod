2b4dcda9c6eed75b12ce7c0e69ad712e931de044
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-06-17 13:15:33 +0200

    Initial commit - Mirrored from my site, file: siege_mod_1710/src/main/java/hu/AfghanGoat/SiegeMod/AGLib.java

---
+++ siege_mod_1710/src/main/java/hu/AfghanGoat/SiegeMod/AGLib.java
@@ -0,0 +1,10 @@
+package hu.AfghanGoat.SiegeMod;
+
+public class AGLib {
+    public static final String ModID = "siege_mod";
+    public static final String ModName = "AfghanGoat's Siege Mod 1.7.10";
+    public static final String Version = "v1.0";
+    public static final String CLIENT_PROXY = "hu.AfghanGoat.SiegeMod.proxy.ClientProxy";
+    public static final String COMMON_PROXY = "hu.AfghanGoat.SiegeMod.proxy.CommonProxy";
+    public static final String tabBlocksName = ModID + "_" + "tabBlocks";
+}
